180491ce2c556aad97d2cde4919aaa6eeae2f21c
parasite-e <fardinemonbd@gmail.com>
2025-03-27 00:58:05 +0600

    params configured in the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import logging
 from sklearn.model_selection import train_test_split
+import yaml
 
 # creating log directory
 
@@ -27,6 +28,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+def load_params(params_path: str) -> dict:
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameter retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('Yaml error %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error found %s', e)
+        raise
 
 
 def load_data(data_url: str) -> pd.DataFrame:
@@ -75,7 +93,8 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
         data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
